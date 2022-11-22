b5b954e37d0e3343085ed92b206e90a85f83f1a1
Igor Lira <igorlirap@gmail.com>
2022-11-22 20:13:09 -0300

    set up database connection - testing

---
+++ web-customer-tracker/src/igor/testdb/TestDB.java
@@ -0,0 +1,49 @@
+package igor.testdb;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestDB
+ */
+@WebServlet("/TestDB")
+public class TestDB extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestDB() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String user = "root";
+		String pass = "root";
+		String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false&serverTimezone=UTC";
+		String driver = "com.mysql.jdbc.Driver";
+		try {
+			PrintWriter out = response.getWriter();
+			out.println("Connecting to database: " + jdbcUrl);
+			Class.forName(driver);
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+			out.println("SUCCESS");
+			myConn.close();
+		}catch(Exception exc) {
+			exc.printStackTrace();
+			throw new ServletException(exc);
+		}
+	}
+
+}
